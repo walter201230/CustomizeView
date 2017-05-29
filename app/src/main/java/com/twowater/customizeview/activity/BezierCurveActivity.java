914fd238ca4e41f368336a8d7a3e122169d3b474
twowater <347073565@qq.com>
2017-05-29 15:12:46 +0800

    贝塞尔曲线

--- app/src/main/java/com/twowater/customizeview/activity/BezierCurveActivity.java
+++ app/src/main/java/com/twowater/customizeview/activity/BezierCurveActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CompoundButton;
-import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,62 +11,28 @@ import android.widget.Toast;
 import com.twowater.beziercurve.BezierCurve;
 import com.twowater.customizeview.R;
 
-public class BezierCurveActivity extends AppCompatActivity {
+public class BezierCurveActivity extends AppCompatActivity implements
+        CompoundButton.OnCheckedChangeListener {
 
     private BezierCurve mBezierCurve;
-
-    private SeekBar mSeekBar;
-
     private TextView mTextView;
-
     private Switch mLoop, mTangent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bezier_curve);
+        initView();
+    }
+
+    private void initView() {
         mBezierCurve = (BezierCurve) findViewById(R.id.bezier);
         mTextView = (TextView) findViewById(R.id.textview);
-        mSeekBar = (SeekBar) findViewById(R.id.seekbar);
         mLoop = (Switch) findViewById(R.id.loop);
         mTangent = (Switch) findViewById(R.id.tangent);
-
         mTextView.setText(mBezierCurve.getOrderStr() + "阶贝塞尔曲线");
-
-        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (progress == 0) {
-                    progress = 1;
-                }
-                mBezierCurve.setRate(progress * 2);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
         mLoop.setChecked(false);
         mTangent.setChecked(true);
-        mLoop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                mBezierCurve.setLoop(isChecked);
-            }
-        });
-        mTangent.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                mBezierCurve.setTangent(isChecked);
-            }
-        });
     }
 
     public void start(View view) {
@@ -91,6 +56,18 @@ public class BezierCurveActivity extends AppCompatActivity {
             mTextView.setText(mBezierCurve.getOrderStr() + "阶贝塞尔曲线");
         } else {
             Toast.makeText(this, "删除失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        switch (compoundButton.getId()) {
+            case R.id.loop:
+                mBezierCurve.setLoop(isChecked);
+                break;
+            case R.id.tangent:
+                mBezierCurve.setTangent(isChecked);
+                break;
         }
     }
 }
