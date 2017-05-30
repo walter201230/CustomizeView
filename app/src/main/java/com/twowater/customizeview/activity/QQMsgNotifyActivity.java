8e5ae69aa58fec0a09b24a8b14f73ccfd5852fe4
twowater <347073565@qq.com>
2017-05-31 01:17:15 +0800

    QQ消息提醒红点

--- app/src/main/java/com/twowater/customizeview/activity/QQMsgNotifyActivity.java
+++ app/src/main/java/com/twowater/customizeview/activity/QQMsgNotifyActivity.java
@@ -1,15 +1,43 @@
 package com.twowater.customizeview.activity;
 
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.twowater.customizeview.R;
+import com.twowater.qqmsgnotify.GooViewListener;
 
 public class QQMsgNotifyActivity extends AppCompatActivity {
+
+    private TextView mTvPoint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qq_msg_notify);
+        initView();
     }
+
+    private void initView() {
+        mTvPoint = (TextView) findViewById(R.id.point_conversation);
+        mTvPoint.setText("10");
+        mTvPoint.setTag(10);
+        GooViewListener listener = new GooViewListener(this, mTvPoint) {
+            @Override
+            public void onDisappear(PointF mDragCenter) {
+                super.onDisappear(mDragCenter);
+                Toast.makeText(QQMsgNotifyActivity.this, "消失了", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onReset(boolean isOutOfRange) {
+                super.onReset(isOutOfRange);
+                Toast.makeText(QQMsgNotifyActivity.this, "重置了", Toast.LENGTH_SHORT).show();
+            }
+        };
+        mTvPoint.setOnTouchListener(listener);
+    }
+
 }
